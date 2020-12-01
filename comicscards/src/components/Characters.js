8abcdc131147c46564d3a907c363539594377dae
Huzzky <vladislav.bychkov01@gmail.com>
2020-12-01 11:59:12 +0300

    init filter, update cards

--- comicscards/src/components/Characters.js
+++ comicscards/src/components/Characters.js
@@ -4,20 +4,16 @@ import { Link, useParams } from 'react-router-dom'
 
 export const Characters = (state) => {
     const param = useParams()
-
     useEffect(() => {
-        // * When a component loading --> fetch data to redux
-        state.getCard('characters', 24, param.pageId - 1)
-
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [param.pageId])
 
     const partingCards = (arrCards) => {
         // * Decomposed on the shelves
-        console.log(arrCards)
         if (arrCards.length > 0) {
             let allCards = arrCards.map((index, key) => (
                 <Link
+                    className="cards"
                     key={key}
                     to={{
                         pathname: '/characters/person/' + index.id,
@@ -32,11 +28,6 @@ export const Characters = (state) => {
                             height="256px"
                         />
                         <h1>{index.name}</h1>
-                        {index.publisher === null ? (
-                            <p>Publisher: Unknow</p>
-                        ) : (
-                            <p>Publisher: {index.publisher.name}</p>
-                        )}
                     </div>
                 </Link>
             ))
@@ -48,9 +39,6 @@ export const Characters = (state) => {
     }
 
     const { arr, isFetchCards } = state.charactersCard
-    console.log(!!Object.keys(arr).length)
-    // if (arr)
-    // const arrResults = arr
     if (isFetchCards) {
         return (
             <div>
@@ -59,50 +47,8 @@ export const Characters = (state) => {
         )
     } else if (!isFetchCards) {
         if (!!Object.keys(arr).length) {
-            console.log(
-                param.pageId,
-                arr.number_of_total_results,
-                arr.number_of_page_results
-            )
             return (
                 <div className="div--characters">
-                    <div className="div div__div">
-                        <Link
-                            to={
-                                '/characters/page=' +
-                                (parseInt(param.pageId) - 1)
-                            }
-                        >
-                            <button
-                                className="btn div__btn"
-                                disabled={param.pageId < 2 ? 'disabled' : ''}
-                            >
-                                {' '}
-                                Туда{' '}
-                            </button>
-                        </Link>
-                        <Link
-                            to={
-                                '/characters/page=' +
-                                (parseInt(param.pageId) + 1)
-                            }
-                        >
-                            <button
-                                className="btn div__btn"
-                                disabled={
-                                    param.pageId <
-                                    Math.ceil(
-                                        arr.number_of_total_results / arr.limit
-                                    )
-                                        ? ''
-                                        : 'disabled'
-                                }
-                            >
-                                {' '}
-                                Сюда{' '}
-                            </button>
-                        </Link>
-                    </div>
                     <div className="grid grid-table">
                         {partingCards(arr.results)}
                     </div>
